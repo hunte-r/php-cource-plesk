3bdc78874eb73a47bebbe451ec7e98ddb246d061
Evgenii Ruzavin <eruzavin@plesk.com>
2021-12-22 11:59:15 +0700

    Implemented function binarySum

---
+++ math.php
@@ -0,0 +1,15 @@
+<?php
+
+function binarySum(string $binA, string $binB): string
+{
+    // Check if input arguments are binary
+    $regex = '/^[01]+$/';
+
+    if ((!preg_match($regex, $binA)) || (!preg_match($regex, $binB))) {
+        return "At least one input argument is not binary\n";
+    }
+
+    $decimalResult = bindec($binA) + bindec($binB);
+
+    return decbin($decimalResult);
+}
