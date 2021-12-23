6033757fa5149f0a93c01ce1491d1079cd43d728
Evgenii Ruzavin <eruzavin@plesk.com>
2021-12-23 17:50:39 +0700

    Implemented function addDigits

--- math.php
+++ math.php
@@ -59,3 +59,26 @@ function fib(int $fibIndex): int
 
     return $fibSeries[$fibIndex];
 }
+    
+function addDigits(int $number)
+{
+    if ($number < 0) {
+        return "Negative numbers are not allowed";
+    }
+
+    // Split input number to digits and put them to array
+    $digitsArray = str_split((string)$number);
+    
+    // If the number contains only one digit, return the number as is
+    if (count($digitsArray) === 1) {
+        return $number;
+    }
+
+    $result = 0;
+    while (count($digitsArray) > 1) {
+        $result = array_sum($digitsArray);
+        $digitsArray = str_split((string)$result);
+    }
+    
+    return $result;
+}
