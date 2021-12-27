5f6dbd3cae765001423c007660b12933fba7259e
Evgenii Ruzavin <eruzavin@plesk.com>
2021-12-27 20:14:01 +0700

    Implemented function isBalanced

--- math.php
+++ math.php
@@ -82,3 +82,24 @@ function addDigits(int $number)
     
     return $result;
 }
+
+function isBalanced(string $string): bool
+{
+    if ($string === '') {
+        return true;
+    }
+
+    // Check if input string contains only round brackets
+    $regex = '/^[()]+$/';
+    if (!preg_match($regex, $string)) {
+        throw new \ValueError('Incorrect format of the input string');
+    }
+
+    $position = strpos($string, '()');
+    while ($position !== false) {
+        $string = substr_replace($string, '', $position, 2);
+        $position = strpos($string, '()');
+    }
+    
+    return $string === '';
+}
